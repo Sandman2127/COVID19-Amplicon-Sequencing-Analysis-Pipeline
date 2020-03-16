a2fbc146fd736f0ea393f4b290d77bbe65600dce
Dean Sanders <dmsanders@wisc.edu>
2020-03-16 17:53:20 -0500

    Adds wget install to the environment so users can wget without issue, many local installs will already have it, but this should simplify the problem

--- condaEnv/create_env.sh
+++ condaEnv/create_env.sh
@@ -1,4 +1,5 @@
 conda create -n COVID19-Amplicon-Seq -y python=3.7
+conda install -c anaconda -n COVID19-Amplicon-Seq -y wget 
 conda install -c bioconda -n COVID19-Amplicon-Seq -y sabre
 conda install -c bioconda -n COVID19-Amplicon-Seq -y bowtie2
 conda install -c bioconda -n COVID19-Amplicon-Seq -y samtools=1.9
